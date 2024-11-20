ad951c17d1328841f3d7df1941a7e7ee54713d9c
João <almeiida.joao@gmail.com>
2024-11-20 14:57:24 -0300

    criação de rotas

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,16 +6,18 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'inicio',
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: '/sobre',
+      name: 'sobre',
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
     },
   ],
 })
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="sobre">
+      <h1>This is an login page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
